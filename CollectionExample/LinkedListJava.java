cdbddd57c630e9728d7fe44d967d532259b5da9c
VX <vineethxavier89@gmail.com>
2021-01-21 00:16:08 -0700

    added comments jan 21

--- CollectionExample/LinkedListJava.java
+++ CollectionExample/LinkedListJava.java
@@ -1,7 +1,13 @@
 package CollectionExample;
 
+/**
+ * @author vineethxavier
+ *
+ * {@link LinkedListJava}
+ */
 public class LinkedListJava {
     public static void main(String[] args) {
         System.out.println("====linked list examples====");
+
     }
 }
